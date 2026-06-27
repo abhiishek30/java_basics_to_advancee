23d616915fa25e80376c71b5d705b7805f2ff21c
abhiishek30 <abhisheksingh8356@gmail.com>
2026-06-27 12:23:43 +0530

    commit

---
+++ java_programs/loop_pattern_printing10.java
@@ -0,0 +1,12 @@
+public class loop_pattern_printing10 {
+    public static void main(String []args){
+        for(int i=1;i<=3;i++)
+        {
+            for(int j=0;j<=5;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
